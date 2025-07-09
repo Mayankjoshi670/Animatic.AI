95907073c28333e3c7978a9af14fc72180f0d19d
Mayank joshi <147848494+Mayankjoshi670@users.noreply.github.com>
2025-07-10 01:45:58 +0530

    restructuring frontend ---

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import "./index.css"
-import Page from "../page"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import Page from "../page";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Page/>
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Page />
+    </BrowserRouter>
+  </React.StrictMode>
+);
